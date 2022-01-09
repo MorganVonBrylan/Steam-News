5d628b14ba357d911e8790adb2a82cd7ad1341c1
Morgân von Brylân <julesr1230@gmail.com>
2022-01-09 19:38:20 +0100

    Ajoute /derniere

---
+++ commands/derniere.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const { query, toEmbed } = require("../steam_news/api");
+
+exports.description = "Voir la dernière actu d'un jeu";
+exports.options = [{
+	type: "INTEGER", name: "id",
+	description: "L'id du jeu (le numéro après app/ dans l'URL de la page du magasin)", required: true
+}];
+exports.run = inter => {
+	query(inter.options.getInteger("id"), 10).then(({appnews}) => {
+		if(!appnews)
+			return inter.reply({content: "L'id que vous avez fourni ne correspond à aucune appli Steam.", ephemeral: true}).catch(error);
+		if(!appnews.newsitems.length)
+			return inter.reply({content: "Cette application n'a aucune actu.", ephemeral: true}).catch(error);
+
+		inter.reply({ embeds: [toEmbed(appnews.newsitems[0])] }).catch(error);
+	})
+}
