f00eb764e82277671e8a715ce9921df97a28a84f
Morgân von Brylân <julesr1230@gmail.com>
2022-01-10 05:03:40 +0100

    /derniere ignore désormais les actus non Steam.

--- commands/derniere.js
+++ commands/derniere.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const { query } = require("../steam_news/api");
+const { query, isSteamNews } = require("../steam_news/api");
 const toEmbed = require("../steam_news/toEmbed.function");
 
 exports.description = "Voir la dernière actu d'un jeu";
@@ -15,6 +15,10 @@ exports.run = inter => {
 		if(!appnews.newsitems.length)
 			return inter.reply({content: "Cette application n'a aucune actu.", ephemeral: true}).catch(error);
 
-		inter.reply({ embeds: [toEmbed(appnews.newsitems[0])] }).catch(error);
+		for(const news of appnews.newsitems)
+			if(isSteamNews(news))
+				return inter.reply({ embeds: [toEmbed(news)] }).catch(error);
+
+		inter.reply({content: "Cette application n'a aucune actu récente.", ephemeral: true}).catch(error);
 	})
 }
