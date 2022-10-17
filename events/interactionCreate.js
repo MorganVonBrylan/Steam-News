32542edfc5510d2017a426b06c27548aa539ab60
Morgân von Brylân <julesr1230@gmail.com>
2022-10-17 20:08:54 +0200

    Removes obsolete checking about server-only commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,17 +8,10 @@ module.exports = exports = interaction => {
 	const command = commands[interaction.commandName];
 
 	if(interaction.type === APPLICATION_COMMAND_AUTOCOMPLETE)
-	{
-		return interaction.inGuild() || !command.dmPermission
-		 	? command.autocomplete(interaction)
-			: interaction.respond([{name: "This command only works in servers.", value: "N/A"}]).catch(Function());
-	}
+		return command.autocomplete(interaction);
 
 	if(interaction.type !== APPLICATION_COMMAND)
 		return;
-
-	if(!interaction.inGuild() && !command.global)
-		return interaction.reply({ephemeral: true, content: "This command only works in servers."}).catch(error);
 
 	if(command)
 		command.run(interaction);
