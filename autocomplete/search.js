1955b9f1fea8c2eff01944a40389e590730f2d99
Morgân von Brylân <julesr1230@gmail.com>
2022-12-04 15:34:19 +0100

    autocomplete: fixes too long game names

--- autocomplete/search.js
+++ autocomplete/search.js
@@ -4,6 +4,6 @@ const { search } = require("../steam_news/api");
 
 module.exports = exports = inter => {
 	search(inter.options.getFocused()).then(results => {
-		inter.respond(results.map(({id, name}) => ({ name, value: ""+id }))).catch(error);
+		inter.respond(results.map(({id, name}) => ({ name: name.length > 100 ? name.substring(0, 99) + "…" : name, value: ""+id }))).catch(error);
 	})
 }
