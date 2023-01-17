5fa4b03b1418d4bf7dd4d16bf4d861987f35ffa8
Morgân von Brylân <julesr1230@gmail.com>
2023-01-17 10:07:09 +0100

    Updates the help

--- commands/help.js
+++ commands/help.js
@@ -1,25 +1,35 @@
 "use strict";
 
-const { WATCH_LIMIT } = require("../steam_news/limits");
+const { WATCH_LIMIT, WATCH_VOTE_BONUS } = require("../steam_news/limits");
 const SUPPORT_SERVER = require("../bot").auth.supportServer || "*(no support server invite set)*";
 const {repository: {url: repository}, version, author} = require("../package.json");
+const { voteURL } = require("../dbl");
+
+const preparedEmbeds = new Set();
 
 exports.dmPermission = true;
 exports.run = inter => {
 	const {myself, master} = require("../bot");
-	const embed = {
-		...tr.get(inter.locale, "help"),
-		thumbnail: { url: myself.avatarURL() },
-	};
-	embed.footer.icon_url = master.avatarURL({dynamic: true});
-	embed.footer.text = embed.footer.text.replace("${v}", version).replace("${author}", author);
+	const {locale} = inter;
 
-	for(const field of embed.fields)
+	const embed = tr.get(locale, "help");
+	embed.thumbnail = { url: myself.avatarURL() };
+	embed.footer.icon_url = master.avatarURL({dynamic: true});
+
+	if(!preparedEmbeds.has(locale))
 	{
-		field.value = field.value
-			.replaceAll("${WATCH_LIMIT}", WATCH_LIMIT)
-			.replace("${SUPPORT_SERVER}", SUPPORT_SERVER)
-			.replaceAll("${repository}", repository);
+		embed.footer.text = embed.footer.text.replace("${v}", version).replace("${author}", author);
+		for(const field of embed.fields)
+		{
+			field.value = field.value
+				.replace("${WATCH_LIMIT}", WATCH_LIMIT)
+				.replace("${WATCH_VOTE_BONUS}", WATCH_VOTE_BONUS)
+				.replace("${SUPPORT_SERVER}", SUPPORT_SERVER)
+				.replaceAll("${repository}", repository)
+				.replace("${VOTE}", voteURL(locale));
+		}
+
+		preparedEmbeds.add(locale);
 	}
 
 	inter.reply({ ephemeral: true, content: SUPPORT_SERVER, embeds: [embed] }).catch(error);
