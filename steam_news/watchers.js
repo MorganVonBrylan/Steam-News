44ed237732b765ca3a1672d34a43f1875037cf97
Morgân von Brylân <julesr1230@gmail.com>
2023-05-17 10:03:33 +0200

    Fixes a checkPrice fail

--- steam_news/watchers.js
+++ steam_news/watchers.js
@@ -189,8 +189,10 @@ async function checkPrices()
 	for(const appDetails of stmts.findWatchedPrices())
 		watchedPrices[appDetails.appid] = appDetails;
 
-	for(const [appid, {final, discount_percent}] of Object.entries(await queryPrices(Object.keys(watchedPrices))))
+	for(const [appid, {final, discount_percent}= {}] of Object.entries(await queryPrices(Object.keys(watchedPrices))))
 	{
+		if(!final) continue; // Sometimes the API call just... fails
+
 		const lastKnownPrice = watchedPrices[appid].lastPrice;
 		if(final === lastKnownPrice)
 			continue;
