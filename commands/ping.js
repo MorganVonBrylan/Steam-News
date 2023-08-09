c4bf08106de843df8bc8f4ac1a63b076d60ffaad
Morgân von Brylân <julesr1230@gmail.com>
2023-08-09 03:16:09 +0200

    Adds a /ping command

---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+"use strict";
+
+exports.dmPermission = true;
+exports.description = "Pong!";
+exports.run = async inter => {
+	inter.reply({ephemeral: true, content: `Pong! (${inter.client.ws.ping}ms)`}).catch(error);
+}
