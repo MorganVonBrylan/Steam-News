6c7fa85deb9977ebbc7b0f10edf5d5214ac26b01
Morgân von Brylân <julesr1230@gmail.com>
2023-08-18 14:33:55 +0200

    Ignores unidici (fetchà connection fails

--- error.js
+++ error.js
@@ -12,7 +12,8 @@ global.error = module.exports = exports = function error(err)
 		const status = err.httpStatus || err.response?.status || err.response?.statusCode;
 		if(message === "read ECONNRESET" || status === 403 || status === 404 || status === 408 || status >= 500
 			|| message === "Unknown interaction" || message === "Missing Access"
-			|| message.startsWith("invalid json response body"))
+			|| message.startsWith("invalid json response body")
+			|| err.code === "UND_ERR_CONNECT_TIMEOUT")
 			return;
 
 		msg += err.name === "DiscordAPIError" ? `\nMessage : ${message}\nChemin : ${err.path}` : `\nMessage : ${message}`;
