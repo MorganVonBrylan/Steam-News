a7fa90d932a7d3f9557ed37ca70057eca69bf44a
Morgân von Brylân <julesr1230@gmail.com>
2025-06-17 09:19:34 +0200

    feat[tr]: more warnings for missing translations

--- localization/applyTranslations.function.js
+++ localization/applyTranslations.function.js
@@ -4,11 +4,20 @@ export default function applyTranslations(cmdName, cmd)
 	const {FALLBACK, locales} = this;
 	for(const [locale, {commands}] of Object.entries(locales))
 	{
-		if(!commands || !(cmdName in commands))
+		if(!commands)
+		{
+			console.warn(`Missing ${locale} command translations`);
 			continue;
+		}
 
 		if(locale === FALLBACK)
 		{
+			if(!(cmdName in commands))
+			{
+				console.warn(`Missing fallback translation for command ${cmdName}`);
+				continue;
+			}
+
 			const {description, options} = commands[cmdName];
 			if(!description)
 				console.warn(`Missing fallback description for command ${cmdName}`);
@@ -21,59 +30,97 @@ export default function applyTranslations(cmdName, cmd)
 					console.warn(`Missing fallback option translations for command ${cmdName}`);
 				else for(const opt of cmd.options)
 				{
+					const forOption = `for option ${opt.name} of command ${cmdName}`;
 					const tr = options[opt.name];
 					if(!tr)
-						console.warn(`Missing fallback translation for option ${opt.name} of command ${cmdName}`);
+						console.warn(`Missing fallback translation ${forOption}`);
 					else
 					{
-						const { description } = tr;
+						const { description, choices } = tr;
 						if(!description)
-							console.warn(`Missing fallback description for option ${opt.name} of command ${cmdName}`);
+							console.warn(`Missing fallback description ${forOption}`);
 						else
 							opt.description = description;
+
+						const nChoices = opt.choices?.length;
+
+						if(nChoices && nChoices !== choices?.length)
+						{
+							console.warn(`Mismatched number of fallback choices ${forOption} (expected ${nChoices}, got ${choices?.length})`);
+						}
 					}
 				}
 			}
 			continue;
 		}
 
-		const {name, description, options} = commands[cmdName];
-
-		if(cmd.nameLocalizations) cmd.nameLocalizations[locale] = name;
-		else cmd.nameLocalizations = { [locale]: name };
-		if(cmd.descriptionLocalizations) cmd.descriptionLocalizations[locale] = description;
-		else cmd.descriptionLocalizations = { [locale]: description };
-
-		if(!cmd.options?.length)
-			continue;
-		if(!options)
+		if(!(cmdName in commands))
 		{
-			console.warn(`Missing ${locale} option translations for command ${cmdName}`);
+			console.warn(`Missing ${locale} translation for command ${cmdName}`);
 			continue;
 		}
 
-		for(const opt of cmd.options)
+
+		const { name, description, options } = commands[cmdName];
+
+		if(!name)
+			console.warn(`Missing ${locale} name translation for command ${cmdName}`);
+		else
 		{
-			const tr = options[opt.name];
+			if(cmd.nameLocalizations) cmd.nameLocalizations[locale] = name;
+			else cmd.nameLocalizations = { [locale]: name };
+		}
+
+		if(!description)
+			console.warn(`Missing ${locale} description translation for command ${cmdName}`);
+		else
+		{
+			if(cmd.descriptionLocalizations) cmd.descriptionLocalizations[locale] = description;
+			else cmd.descriptionLocalizations = { [locale]: description };
+		}
+
+		if(!cmd.options?.length)
+			continue;
+
+		if(!options)
+			console.warn(`Missing ${locale} option translations for command ${cmdName}`);
+		else for(const opt of cmd.options)
+		{
+			const forOption = `translation for option ${opt.name} of command ${cmdName}`;
+			let tr = options[opt.name];
 			if(!tr)
 			{
-				console.warn(`Missing ${locale} translation for option ${opt.name} of command ${cmdName}`);
+				console.warn(`Missing ${locale} ${forOption}`);
 				continue;
 			}
 
-			const {name, description, choices} = tr;
-			if(opt.nameLocalizations) opt.nameLocalizations[locale] = name;
-			else opt.nameLocalizations = { [locale]: name };
-			if(opt.descriptionLocalizations) opt.descriptionLocalizations[locale] = description;
-			else opt.descriptionLocalizations = { [locale]: description };
+			let { name, description, choices } = tr;
 
-			if(!opt.choices?.length)
+			if(!name)
+				console.warn(`Missing ${locale} name ${forOption}`);
+			else
+			{
+				if(opt.nameLocalizations) opt.nameLocalizations[locale] = name;
+				else opt.nameLocalizations = { [locale]: name };
+			}
+
+			if(!description)
+				console.warn(`Missing ${locale} description ${forOption}`);
+			else
+			{
+				if(opt.descriptionLocalizations) opt.descriptionLocalizations[locale] = description;
+				else opt.descriptionLocalizations = { [locale]: description };
+			}
+
+			const nChoices = opt.choices?.length;
+			if(!nChoices)
 				continue;
 
-			if(opt.choices.length !== choices?.length)
-				console.warn(`Mismatched number of choices in ${locale} translation for option ${opt.name} of command ${cmdName} (expected ${opt.choices.length}, got ${choices?.length})`);
-
-			for(let i = 0 ; i < choices.length ; ++i)
+			if(nChoices !== choices?.length)
+			{
+				console.warn(`Mismatched number of choices in ${locale} ${forOption} (expected ${nChoices}, got ${choices?.length})`);
+			}
+			else for(let i = 0 ; i < nChoices ; ++i)
 			{
 				const choice = opt.choices[i];
 				if(choice.nameLocalizations) choice.nameLocalizations[locale] = choices[i];
