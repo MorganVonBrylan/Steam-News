5b7a9ba1413f6cc7286543ceb8204ebfeb5e338b
Morgân von Brylân <julesr1230@gmail.com>
2025-08-27 20:38:22 +0200

    feat[error]: simplify the logging of silent errors

--- utils/error.js
+++ utils/error.js
@@ -16,34 +16,38 @@ export function error(err)
 	if(err instanceof SyntaxError) // from an import probably
 		throw err;
 	
-	if(
-		err instanceof DOMException // Discord.js failing some random call
+	if(!err
+		|| err instanceof DOMException // Discord.js failing some random call
 		|| err instanceof TopGGAPIError && err.response.statusCode === 429 // occasional Top.gg bug
 	)
 		return;
+
+	const { message } = err;
+	const status = err instanceof TopGGAPIError ? err.response?.statusCode
+		: (err.httpStatus || err.response?.status || err.response?.statusCode || err.code);
+	const code = err.code || err.cause?.code;
+
+	if(message === "read ECONNRESET"
+		|| status === 403 || status === 404 || status === 408 || status >= 500
+		|| message === "Unknown interaction" || message === "Missing Access"
+		|| message.startsWith("invalid json response body")
+		|| code === "UND_ERR_CONNECT_TIMEOUT"
+		|| code === "UND_ERR_ABORTED"
+		|| code === "UND_ERR_SOCKET")
+	{
+		console.error(new Date(), message);
+		return;
+	}
 	
 	console.error(new Date(), err);
 	let msg = "An error occurred; read the console for details.";
 
-	if(err?.message)
+	if(message)
 	{
-		const {message} = err;
-		const status = err.httpStatus || err.response?.status
-					 || err.response?.statusCode || err.code;
-		const code = err.code || err.cause?.code;
-		if(message === "read ECONNRESET"
-			|| status === 403 || status === 404 || status === 408 || status >= 500
-			|| message === "Unknown interaction" || message === "Missing Access"
-			|| message.startsWith("invalid json response body")
-			|| code === "UND_ERR_CONNECT_TIMEOUT"
-			|| code === "UND_ERR_ABORTED"
-			|| code === "UND_ERR_SOCKET")
-			return;
-
 		if(err instanceof DiscordAPIError)
 		{
 			msg += `\nMessage : ${message}\nPath : ${err.path || err.url}`;
-			console.log("data:", err.requestBody?.json?.data);
+			console.error("data:", err.requestBody?.json?.data);
 		}
 		else
 			msg += `\nMessage : ${message}`;
