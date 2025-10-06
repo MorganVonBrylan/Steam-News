45db7b8833cc327b22a8cd03742f9d674a8a4f48
Morgân von Brylân <julesr1230@gmail.com>
2025-10-06 13:41:48 +0200

    feat: admin test command

---
+++ commands/owner/test.js
@@ -0,0 +1,30 @@
+
+
+export const description = "Test a message";
+export const options = [{
+	type: STRING, name: "message", required: true,
+	description: "The message content or embed to reply with.",
+}];
+export function run(inter) {
+    let message = inter.options.getString("message");
+    try {
+        message = JSON.parse(message);
+    } catch {
+        message = { content: message };
+    }
+
+    if("flags" in message)
+    {
+        if(message.flags instanceof Array)
+        {
+            if(!message.flags.includes("Ephemeral"))
+                message.flags.push("Ephemeral");
+        }
+        else if(message.flags !== "Ephemeral")
+            message.flags = [message.flags, "Ephemeral"];
+    }
+    else
+        message.flags = "Ephemeral";
+    
+    inter.reply(message);
+}
