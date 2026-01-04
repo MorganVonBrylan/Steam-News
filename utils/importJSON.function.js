4044b095ffd787e7bdbe10834a91dc2ef68534be
Morgân von Brylân <julesr1230@gmail.com>
2026-01-04 16:09:50 +0100

    feat[importJSON]: accept a default value, and throw is there isn't one and the file doesn't exist

--- utils/importJSON.function.js
+++ utils/importJSON.function.js
@@ -1,5 +1,5 @@
 
-import { readFileSync } from "node:fs";
+import { existsSync, readFileSync } from "node:fs";
 import { resolve } from "node:path";
 
 const cache = new Map();
@@ -7,15 +7,26 @@ const cache = new Map();
 /**
  * Import a JSON file like require, except relative paths are relative to the working directory instead of the current file.
  * @param {string} path The .json file's path
+ * @param {*} defaultValue The default value to use in case the file doesn't exist. If left undefined, the function will throw instead.
  * @returns {*} The parsed JSON file
  */
-export default function importJSON(path)
+export default function importJSON(path, defaultValue)
 {
-    path = resolve(path);
-    if(cache.has(path))
-        return cache.get(path);
+	path = resolve(path);
+	if(cache.has(path))
+		return cache.get(path);
 
-    const json = JSON.parse(readFileSync(path));
-    cache.set(path, json);
-    return json;
+	let value;
+	if(existsSync(path))
+		value = JSON.parse(readFileSync(path));
+	else
+	{
+		if(defaultValue !== undefined)
+			value = defaultValue;
+		else
+			throw new Error(`The file '${path}' does not exist.`);
+	}
+
+	cache.set(path, value);
+	return value;
 }
