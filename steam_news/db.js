d93648cf6050fdfcec613dbe92457c9307010378
Morgân von Brylân <julesr1230@gmail.com>
2026-03-12 13:31:41 +0100

    fix: set a game's 'latest' field to NULL if it is no longer watched This avoids a bug where a game becomes unwatched, keeps an outated 'latest' value, and floods the next channel that watches is with old news

--- steam_news/db.js
+++ steam_news/db.js
@@ -198,6 +198,7 @@ export const stmts = {
 		ORDER BY name`),
 	updateLatest: db.prepare("UPDATE Apps SET latest = $latest WHERE appid = $appid"),
 	getWatcherChannel: db.prepare("SELECT channelId from Watchers WHERE appid = $appid AND guildId = $guildId").pluck(),
+	isWatched: db.prepare("SELECT EXISTS(SELECT 1 FROM Watchers WHERE appid = ?)").pluck(),
 
 	watchSteam: makeProxy([
 		"UPDATE SteamWatchers SET channelId = $channelId, roleId = $roleId WHERE guildId = $guildId",
@@ -208,6 +209,7 @@ export const stmts = {
 	getSteamWatcher: db.prepare("SELECT channelId FROM SteamWatchers WHERE guildId = ?").pluck(),
 	unwatchSteam: db.prepare("DELETE FROM SteamWatchers WHERE guildId = ?"),
 	getSteamWatchers: db.prepare("SELECT * FROM SteamWatchers"),
+	isSteamWatched: db.prepare("SELECT EXISTS(SELECT 1 FROM SteamWatchers)").pluck(),
 
 	watchPrice: db.prepare("INSERT INTO PriceWatchers (appid, guildId, channelId, roleId, premium) VALUES ($appid, $guildId, $channelId, $roleId, $premium)"),
 	unwatchPrice: db.prepare("DELETE FROM PriceWatchers WHERE appid = ? AND guildid = ?"),
