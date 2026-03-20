f18fbb7f35d9c3670ead7483475e85e34ee7d92f
Morgân von Brylân <julesr1230@gmail.com>
2026-03-20 14:35:17 +0100

    feat[embeds]: FieldList

--- utils/embeds.js
+++ utils/embeds.js
@@ -15,7 +15,7 @@ const EMBEDS_PER_MESSAGE = 10;
 
 /**
  * Send embeds to a channel or interaction while respecting the embed limit per message.
- * @param {object[]} embeds The embeds
+ * @param {object[]|FieldList} embeds The embeds
  * @param {ChatInputCommandInteraction|import("./channels.js").GuildTextChannel} sender 
  * @param {import("discord.js").BitFieldResolvable} [flags]
  */
@@ -48,4 +48,37 @@ export async function sendEmbeds(embeds, sender, flags)
 	}
 	else for(let i = 0 ; i < embeds.length ; i += EMBEDS_PER_MESSAGE)
 		await sender.send(slice(i));
+}
+
+
+/**
+ * This class allows having groups of fields sprawl over several embeds.
+ * It is, fundamentally, an array of embeds.
+ */
+export class FieldList extends Array
+{
+	/**
+	 * Add a group of fields to the list.
+	 * @param {{name:string, value:string}} fields The fields to add
+	 * @param {object} [props] Additional properties for the embed.
+	   * @param {string} [props.title] Title to give to this group of fields
+	   * @param {string} [props.description] Description to give to this group of fields
+	   * @param {number} [props.color] Color for the embeds
+	 * @returns The new length of the field list
+	 */
+	addGroup(fields, { title, description, color } = {}) {
+		if(!fields?.length)
+			return;
+
+		for(const field of fields)
+			if(field.value.length > FIELD_LENGTH_LIMIT)
+				field.value = `${field.value.slice(0, FIELD_LENGTH_LIMIT-3)}…`;
+
+		const firstOfGroup = this.length;
+		for(let i = 0 ; i < fields.length ; i += FIELDS_PER_EMBED)
+			this.push({fields: fields.slice(i, i + FIELDS_PER_EMBED), color});
+		this[firstOfGroup].title = title;
+		this[firstOfGroup].description = description;
+		return this.length;
+	}
 }
