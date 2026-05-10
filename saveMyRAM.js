e6a68e6e7b161f7408f8366270b3868443dea7cf
Morgân von Brylân <julesr1230@gmail.com>
2026-05-10 11:48:02 +0200

    fix: command mentions I'll make djs-command save the command ids later

--- saveMyRAM.js
+++ saveMyRAM.js
@@ -26,7 +26,7 @@ import {
 
 export const cacheLimits = Options.cacheWithLimits({
 	...Options.DefaultMakeCacheSettings,
-	ApplicationCommandManager: 0, // djs-commands has its own cache
+	// No ApplicationCommandManager, we need the command ids for command mentions
 	UserManager: 0,
 	GuildMemberManager: 0,
 	ThreadMemberManager: 0,
