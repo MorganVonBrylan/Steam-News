b9525499b50f1ef537881c32eeef1a19b6cc16ad
Morgân von Brylân <julesr1230@gmail.com>
2026-05-16 23:53:15 +0200

    feat[/watched]; remove the id, which is useless to the user Also improve the code and documentation somewhat

--- commands/watched.js
+++ commands/watched.js
@@ -6,10 +6,10 @@ import { sendEmbeds } from "../utils/embeds.js";
 /** @param {import("discord.js").ChatInputCommandInteraction} inter */
 export function run(inter) {
 	const t = tr.set(inter.locale, "watched");
-	const { guild } = inter;
-	const watched = getWatchedApps(guild.id, true);
+	const { guildId } = inter;
+	const watched = getWatchedApps(guildId, true);
 	const steamWatch = watched.at(-1)?.appid === STEAM_APPID ? watched.pop() : null;
-	const watchedPrices = getWatchedPrices(guild.id);
+	const watchedPrices = getWatchedPrices(guildId);
 
 	const embeds = [
 		...toEmbeds(watched, t, "games"),
@@ -29,21 +29,21 @@ export function run(inter) {
 }
 
 
+const BLOCK_SIZE = 25;
 /**
  * Groups watcher data into embeds.
  * @param {(import("../steam_news/db.js").NewsWatcher|import("../steam_news/db.js").PriceWatcher)[]} watched Watcher data
  * @param {function} t A translator function.
  * @param {"games"|"prices"} type The embed type.
- * @param {number} blockSize How many fields per embed (default and maximum: 25)
  * @returns One or more embeds
  */
-function toEmbeds(watched, t, type, blockSize = 25)
+function toEmbeds(watched, t, type)
 {
 	if(!watched.length) return [];
 	watched = watched.map(watcherToField.bind(null, t));
 	const embeds = [];
-	for(let i = 0 ; i < watched.length ; i += blockSize)
-		embeds.push({fields: watched.slice(i, i + blockSize)});
+	for(let i = 0 ; i < watched.length ; i += BLOCK_SIZE)
+		embeds.push({fields: watched.slice(i, i + BLOCK_SIZE)});
 
 	Object.assign(embeds[0], {
 		title: t(`${type}-watched`),
@@ -54,16 +54,15 @@ function toEmbeds(watched, t, type, blockSize = 25)
 
 /**
  * Turn a watcher's data info an embed field.
- * @param {function} t A translator function.
+ * @param {(key:string, ...replaces:string[])=>string} t A translator function.
  * @param {import("../steam_news/db.js").NewsWatcher|import("../steam_news/db.js").PriceWatcher} watcher The watcher data 
  * @returns {{name:string, value:string, inline:true}} Field data
  */
-function watcherToField(t, {appid, nsfw, name, channelId, roleId, webhook}) {
+function watcherToField(t, {nsfw, name, channelId, roleId, webhook}) {
 	webhook = webhook ? ` ${t("webhook")}` : "";
 	return {
 		name,
-		value: `${t("id", appid)}
-			${t(`NSFW-${nsfw ? "yes" : "no"}`)}
+		value: `${t(`NSFW-${nsfw ? "yes" : "no"}`)}
 			${t("channel", `<#${channelId}>${webhook}`)}
 			${t("ping")} ${roleId ? `<@&${roleId}>` : `*${t("no-ping")}*`}`,
 		inline: true,
