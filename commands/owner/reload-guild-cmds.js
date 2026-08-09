e180c6fb3cfc0555c3069412286c66fa866d6c81
Morgân von Brylân <julesr1230@gmail.com>
2026-08-09 03:30:14 +0200

    fix[/owner]: reloading commands

--- commands/owner/reload-guild-cmds.js
+++ commands/owner/reload-guild-cmds.js
@@ -9,7 +9,7 @@ export const options = [{
 }];
 export async function run(inter)
 {
-	const guild = inter.client.guilds.fetch(inter.options.getString("guild") || inter.guildId);
+	const guild = await inter.client.guilds.fetch(inter.options.getString("guild") || inter.guildId);
 	if(!guild)
 		return inter.reply({flags: "Ephemeral", content: "Guild not found"});
 
