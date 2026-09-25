929879e96a96be2527ec5fd9bacf6a7c4d9ba25f
Morgân von Brylân <julesr1230@gmail.com>
2026-09-25 15:03:59 +0200

    third time's the charm

--- commands/info.js
+++ commands/info.js
@@ -108,7 +108,7 @@ export async function run(inter)
 			});
 		}
 		else if(err instanceof ApiError)
-			inter.editReply({ flags: "Ephemeral", content: tr.get("api-failed") });
+			inter.editReply({ flags: "Ephemeral", content: tr.get(inter.locale, "api-failed") });
 		else
 		{
 			error(Object.assign(err, { appid, lang, cc }));
